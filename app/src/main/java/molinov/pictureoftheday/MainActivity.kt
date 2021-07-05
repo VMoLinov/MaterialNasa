a87a6559e7f5bf4b466a70ec9004ad25a48e2305
VMolinov <molinov@list.ru>
2021-07-05 17:39:32 +0300

    done

--- app/src/main/java/molinov/pictureoftheday/MainActivity.kt
+++ app/src/main/java/molinov/pictureoftheday/MainActivity.kt
@@ -3,11 +3,14 @@ package molinov.pictureoftheday
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import molinov.pictureoftheday.picture.PictureOfTheDayFragment
+import molinov.pictureoftheday.util.THEME
+import molinov.pictureoftheday.util.THEME_INT
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setTheme(shared())
         setContentView(R.layout.main_activity)
         if (savedInstanceState == null) {
             supportFragmentManager.beginTransaction()
@@ -17,5 +20,10 @@ class MainActivity : AppCompatActivity() {
                 ) //Why not fabric method is worked? PictureOfTheDayFragment()
                 .commitNow()
         }
+    }
+
+    private fun shared(): Int {
+        val theme = getSharedPreferences(THEME, MODE_PRIVATE)
+        return theme.getInt(THEME_INT, R.style.App_POD1)
     }
 }
