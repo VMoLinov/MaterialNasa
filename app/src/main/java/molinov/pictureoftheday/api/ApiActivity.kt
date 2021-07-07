c629dcbffae5ba0f885cdaf1ba4fc39fbd53d0a8
VMolinov <molinov@list.ru>
2021-07-07 18:55:00 +0300

    Tabs layout not a center gravity

--- app/src/main/java/molinov/pictureoftheday/api/ApiActivity.kt
+++ app/src/main/java/molinov/pictureoftheday/api/ApiActivity.kt
@@ -1,7 +1,9 @@
 package molinov.pictureoftheday.api
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.viewpager2.widget.ViewPager2
 import com.google.android.material.tabs.TabLayoutMediator
 import molinov.pictureoftheday.R
 import molinov.pictureoftheday.databinding.ActivityApiBinding
@@ -10,6 +12,7 @@ class ApiActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityApiBinding
 
+    @SuppressLint("InflateParams")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityApiBinding.inflate(layoutInflater)
@@ -25,9 +28,22 @@ class ApiActivity : AppCompatActivity() {
                     else -> "EARTH"
                 }
             }.attach()
-            tabLayout.getTabAt(0)?.setIcon(R.drawable.ic_earth)
-            tabLayout.getTabAt(1)?.setIcon(R.drawable.ic_mars)
-            tabLayout.getTabAt(2)?.setIcon(R.drawable.ic_system)
+            tabLayout.getTabAt(0)?.customView =
+                layoutInflater.inflate(R.layout.activity_api_custom_tab_earth, null)
+            tabLayout.getTabAt(1)?.customView =
+                layoutInflater.inflate(R.layout.activity_api_custom_tab_mars, null)
+            tabLayout.getTabAt(2)?.customView =
+                layoutInflater.inflate(R.layout.activity_api_custom_tab_system, null)
+            viewPager.registerOnPageChangeCallback(object : ViewPager2.OnPageChangeCallback() {
+                override fun onPageSelected(position: Int) {
+                    when (position) {
+                        0 -> EarthFragment()
+                        1 -> MarsFragment()
+                        2 -> WeatherFragment()
+                        else -> EarthFragment()
+                    }
+                }
+            })
         }
     }
 }
