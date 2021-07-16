fa5f682e9f1d7b4d4595b487ea51ebd2c3e61b6e
Molinov <molinov@list.ru>
2021-07-16 21:48:49 +0300

    State list animator

--- app/src/main/java/molinov/pictureoftheday/MainActivity.kt
+++ app/src/main/java/molinov/pictureoftheday/MainActivity.kt
@@ -1,6 +1,7 @@
 package molinov.pictureoftheday
 
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.constraintlayout.widget.ConstraintLayout
 import com.google.android.material.bottomnavigation.BottomNavigationView
